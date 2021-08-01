7ee22680803567d22edb8ddfce9b14a3dbb6dbcc
Teshome Ayechiluhem <teshome@ayechiluhem>
2021-08-02 00:28:25 +0300

    working on sidebar section

--- src/Components/Widg/Widg.js
+++ src/Components/Widg/Widg.js
@@ -5,7 +5,7 @@ const Widg = () => {
   return (
     <div className="widgets">
       <div className="header__widgets">
-        <h2>Linkedin News</h2>
+        <h2>Linkedin News latest</h2>
         <Info />
       </div>
     </div>
