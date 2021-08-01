f0ffcf00d155466e776b85a2cd2390904e73c9a0
Teshome Ayechiluhem <teshome@ayechiluhem>
2021-08-02 00:36:22 +0300

    styling widgets section

--- src/Components/Widg/Widg.js
+++ src/Components/Widg/Widg.js
@@ -1,7 +1,19 @@
 import Info from "@material-ui/icons/Info";
 import React from "react";
 import "./Widg.css";
+import { FiberManualRecord } from "@material-ui/icons";
 const Widg = () => {
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets__article">
+      <div className="widgets__articleLeft">
+        <FiberManualRecord />
+      </div>
+      <div className="widgets__articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
   return (
     <div className="widgets">
       <div className="header__widgets">
